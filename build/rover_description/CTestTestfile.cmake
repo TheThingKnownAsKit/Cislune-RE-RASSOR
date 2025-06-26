b1c0fdf82fa10391a433912603ba5b22735450cd
TheThingKnownAsKit <moodyellam@gmail.com>
2025-06-26 09:01:53 -0700

    updated file structure

--- build/rover_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/moody/Cislune/Cislune-RE-RASSOR/src/rover_description
-# Build directory: /home/moody/Cislune/Cislune-RE-RASSOR/build/rover_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
