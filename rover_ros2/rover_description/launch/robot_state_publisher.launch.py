4f55e1ba95456d2deda1165c4e71305466eb5640
TheThingKnownAsKit <moodyellam@gmail.com>
2025-07-01 18:33:57 +0000

    implemented joint state publisher in rsp

--- rover_ros2/rover_description/launch/robot_state_publisher.launch.py
+++ rover_ros2/rover_description/launch/robot_state_publisher.launch.py
@@ -1,23 +1,49 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import Command, PathJoinSubstitution
+from launch.substitutions import Command, PathJoinSubstitution, LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
+
+    # ----- Launch configurations
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_ros2_control = LaunchConfiguration('use_ros2_control')
+
+
+    # ----- Directories
     pkg_share = get_package_share_directory('rover_description')
     xacro_file = PathJoinSubstitution([pkg_share, 'urdf', 'rerassor.xacro.urdf'])
 
     robot_description_content = Command(['xacro ', xacro_file])
     robot_description = ParameterValue(robot_description_content, value_type=str)
 
-    rsp = Node(package='robot_state_publisher',
-             executable='robot_state_publisher',
-             parameters=[{
-                'robot_description': robot_description
-             }],
-             output='screen')
+
+    # ----- Declare common params
+    params = [{
+        'robot_description': robot_description,
+        'use_sim_time': use_sim_time,
+        'use_ros2_control': use_ros2_control
+    }]
+
+    
+    # ----- Nodes
+    rs_pub = Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                output='screen',
+                parameters=params
+                )
+    
+    rjs_pub = Node(
+                package='joint_state_publisher',
+                executable='joint_state_publisher',
+                name='joint_state_publisher',
+                output='screen',
+                parameters=params
+                )
 
     return LaunchDescription([
-        rsp
+        rs_pub,
+        rjs_pub
     ])
--- rover_ros2/rover_description/launch/view_model.launch.py
+++ rover_ros2/rover_description/launch/view_model.launch.py
@@ -8,20 +8,28 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    pkg_share = get_package_share_directory('rover_description')
-    rviz_config_file = PathJoinSubstitution([pkg_share, 'rviz', 'view_model_config.rviz'])
 
-    # Call the separate robot state publisher launcher
+    # ----- Directories
+    pkg_description = get_package_share_directory('rover_description')
+    rviz_config_file = PathJoinSubstitution([pkg_description, 'rviz', 'view_model_config.rviz'])
+
+
+    # ----- Nodes
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([PathJoinSubstitution([(pkg_share), 'launch', 'robot_state_publisher.launch.py'])]),
-        launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([PathJoinSubstitution([(pkg_description), 'launch', 'robot_state_publisher.launch.py'])]),
+        launch_arguments={
+            'use_sim_time': 'true',
+            'use_ros2_control': 'false'
+            }.items()
     )
 
-    rviz = Node(package='rviz2',
-             executable='rviz2',
-             output='screen',
-             name='view_model_rviz',
-             arguments=['-d', rviz_config_file])
+    rviz = Node(
+                package='rviz2',
+                executable='rviz2',
+                output='screen',
+                name='view_model_rviz',
+                arguments=['-d', rviz_config_file]
+                )
 
     return LaunchDescription([
         rsp,
