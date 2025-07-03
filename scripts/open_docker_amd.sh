c77753d118ffc46c95463d268ea9a040eea16281
TheThingKnownAsKit <moodyellam@gmail.com>
2025-07-03 18:52:37 +0000

    added xhost permission commands to open docker scripts

--- scripts/open_docker_amd.sh
+++ scripts/open_docker_amd.sh
@@ -3,6 +3,9 @@ cd docker/
 docker compose -f docker-compose.yaml -f docker-compose.amd.yaml up -d
 cd ..
 
+# Give the container access to computer
+xhost +local:docker
+
 # Open the folder via VS Code CLI
 CID=$(docker ps -qf name=ros2_dev)
 code -r --folder-uri="vscode-remote://attached-container+$(printf '%s' "$CID" | xxd -p)/workspaces/Cislune-RE-RASSOR"
--- scripts/open_docker_nvidia.sh
+++ scripts/open_docker_nvidia.sh
@@ -1,6 +1,9 @@
 # Compose and start the AMD container
 docker compose -f docker-compose.yaml -f docker-compose.nvidia.yaml up -d
 
+# Give the container access to computer
+xhost +local:docker
+
 # Open the folder via VS Code CLI
 CID=$(docker ps -qf name=ros2_dev)
 code --folder-uri="vscode-remote://attached-container+$(printf '%s' "$CID" | xxd -p)/workspaces/Cislune-RE-RASSOR"
