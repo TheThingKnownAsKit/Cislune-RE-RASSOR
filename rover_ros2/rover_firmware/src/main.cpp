0f619f4a06679bdfe80c5fe16d3d3b40afca4a2c
TheThingKnownAsKit <moodyellam@gmail.com>
2025-07-07 17:16:13 +0000

    the micro ros script theoretically works

--- rover_ros2/rover_firmware/src/main.cpp
+++ rover_ros2/rover_firmware/src/main.cpp
@@ -1,136 +1,133 @@
-// #include <rcl/rcl.h>
-// #include <rcl/error_handling.h>
-// #include <rclc/rclc.h>
-// #include <rclc/executor.h>
+#include <Arduino.h>
+#include <micro_ros_platformio.h>  // micro-ROS PlatformIO library
+// micro-ROS and ROS 2 message headers
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <geometry_msgs/msg/twist.h>
 
-// #include <std_msgs/msg/header.h>
+// Motor control pin definitions (TODO ADJUST THESE)
+#define LEFT_DIR_FWD_PIN   2    // left motor forward direction pin
+#define LEFT_DIR_BCK_PIN   3    // left motor backward/reverse pin
+#define LEFT_PWM_PIN       4    // left motor PWM speed pin
+#define RIGHT_DIR_FWD_PIN  5    // right motor forward pin
+#define RIGHT_DIR_BCK_PIN  6    // right motor backward pin
+#define RIGHT_PWM_PIN      7    // right motor PWM pin
 
-// #include <stdio.h>
-// #include <unistd.h>
-// #include <time.h>
+// Robot constants
+const float WHEEL_BASE = 0.30;     // distance between wheels in meters (example)
+const float MAX_LIN_SPEED = 1.0;   // m/s corresponding to full speed
+const int MAX_PWM = 255;
+const float PWM_PER_MPS = (MAX_PWM / MAX_LIN_SPEED);  // PWM increment per 1 m/s of speed
 
-// #define STRING_BUFFER_LEN 100
+// micro-ROS core structures
+rcl_subscription_t cmd_vel_sub;
+geometry_msgs__msg__Twist cmd_vel_msg;
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_node_t node;
+rcl_allocator_t allocator;
 
-// #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc); return 1;}}
-// #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+// Simple error handler macro and LED for debug
+#define RCCHECK(fn)  { rcl_ret_t temp_rc = (fn); if ((temp_rc != RCL_RET_OK)) { errorLoop(); } }
+#define RCSOFTCHECK(fn)  { rcl_ret_t temp_rc = (fn); if ((temp_rc != RCL_RET_OK)) { /* non-fatal error */ } }
+const int LED_PIN = 13;  // Teensy 4.1 on-board LED (pin 13)
 
-// rcl_publisher_t ping_publisher;
-// rcl_publisher_t pong_publisher;
-// rcl_subscription_t ping_subscriber;
-// rcl_subscription_t pong_subscriber;
+void errorLoop() {
+  // Indicate fatal error: blink LED rapidly
+  pinMode(LED_PIN, OUTPUT);
+  while (1) {
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    delay(100);
+  }
+}
 
-// std_msgs__msg__Header incoming_ping;
-// std_msgs__msg__Header outcoming_ping;
-// std_msgs__msg__Header incoming_pong;
+// Callback function to handle incoming Twist messages
+void cmdVelCallback(const void * msgin) {
+  // Cast the incoming ROS message to the Twist type
+  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
+  float linear_x = msg->linear.x;
+  float angular_z = msg->angular.z;
+  
+  // Compute individual wheel speeds (m/s)
+  float left_speed  = linear_x - (angular_z * WHEEL_BASE / 2.0f);
+  float right_speed = linear_x + (angular_z * WHEEL_BASE / 2.0f);
+  
+  // Convert speeds (m/s) to PWM values (0-255)
+  int left_pwm  = (int) (fabs(left_speed) * PWM_PER_MPS);
+  int right_pwm = (int) (fabs(right_speed) * PWM_PER_MPS);
+  if (left_pwm  > MAX_PWM) left_pwm  = MAX_PWM;
+  if (right_pwm > MAX_PWM) right_pwm = MAX_PWM;
+  
+  // Set motor directions
+  if (left_speed >= 0.0f) {
+    digitalWrite(LEFT_DIR_FWD_PIN, HIGH);
+    digitalWrite(LEFT_DIR_BCK_PIN, LOW);
+  } else {
+    digitalWrite(LEFT_DIR_FWD_PIN, LOW);
+    digitalWrite(LEFT_DIR_BCK_PIN, HIGH);
+  }
+  if (right_speed >= 0.0f) {
+    digitalWrite(RIGHT_DIR_FWD_PIN, HIGH);
+    digitalWrite(RIGHT_DIR_BCK_PIN, LOW);
+  } else {
+    digitalWrite(RIGHT_DIR_FWD_PIN, LOW);
+    digitalWrite(RIGHT_DIR_BCK_PIN, HIGH);
+  }
+  
+  // Output PWM to motors
+  analogWrite(LEFT_PWM_PIN, left_pwm);
+  analogWrite(RIGHT_PWM_PIN, right_pwm);
+}
 
-// int device_id;
-// int seq_no;
-// int pong_count;
+void setup() {
+  // Initialize serial transport for micro-ROS
+  Serial.begin(115200);
+  set_microros_serial_transports(Serial);
+  
+  // Initialize on-board LED (for error blink or debugging)
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+  
+  // Initialize motor control pins
+  pinMode(LEFT_DIR_FWD_PIN, OUTPUT);
+  pinMode(LEFT_DIR_BCK_PIN, OUTPUT);
+  pinMode(LEFT_PWM_PIN, OUTPUT);
+  pinMode(RIGHT_DIR_FWD_PIN, OUTPUT);
+  pinMode(RIGHT_DIR_BCK_PIN, OUTPUT);
+  pinMode(RIGHT_PWM_PIN, OUTPUT);
+  // Ensure motors are stopped initially
+  digitalWrite(LEFT_DIR_FWD_PIN, LOW);
+  digitalWrite(LEFT_DIR_BCK_PIN, LOW);
+  digitalWrite(RIGHT_DIR_FWD_PIN, LOW);
+  digitalWrite(RIGHT_DIR_BCK_PIN, LOW);
+  analogWrite(LEFT_PWM_PIN, 0);
+  analogWrite(RIGHT_PWM_PIN, 0);
+  
+  delay(200);  // small delay before micro-ROS init (optional)
+  
+  // Initialize micro-ROS support and node
+  allocator = rcl_get_default_allocator();
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+  RCCHECK(rclc_node_init_default(&node, "teensy41_cmdvel_node", "", &support));
+  
+  // Create a subscriber for the Twist message on /cmd_vel
+  RCCHECK(rclc_subscription_init_default(
+    &cmd_vel_sub,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+    "cmd_vel"
+  ));
+  
+  // Create executor to handle the subscription callback
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  // Note: ON_NEW_DATA executes callback only when new data arrives
+  RCCHECK(rclc_executor_add_subscription(&executor, &cmd_vel_sub, &cmd_vel_msg, &cmdVelCallback, ON_NEW_DATA));
+}
 
-// void ping_timer_callback(rcl_timer_t * timer, int64_t last_call_time)
-// {
-// 	(void) last_call_time;
-
-// 	if (timer != NULL) {
-
-// 		seq_no = rand();
-// 		sprintf(outcoming_ping.frame_id.data, "%d_%d", seq_no, device_id);
-// 		outcoming_ping.frame_id.size = strlen(outcoming_ping.frame_id.data);
-		
-// 		// Fill the message timestamp
-// 		struct timespec ts;
-// 		clock_gettime(CLOCK_REALTIME, &ts);
-// 		outcoming_ping.stamp.sec = ts.tv_sec;
-// 		outcoming_ping.stamp.nanosec = ts.tv_nsec;
-
-// 		// Reset the pong count and publish the ping message
-// 		pong_count = 0;
-// 		RCSOFTCHECK(rcl_publish(&ping_publisher, (const void*)&outcoming_ping, NULL));
-// 		printf("Ping send seq %s\n", outcoming_ping.frame_id.data);  
-// 	}
-// }
-
-// void ping_subscription_callback(const void * msgin)
-// {
-// 	const std_msgs__msg__Header * msg = (const std_msgs__msg__Header *)msgin;
-
-// 	// Dont pong my own pings
-// 	if(strcmp(outcoming_ping.frame_id.data, msg->frame_id.data) != 0){
-// 		printf("Ping received with seq %s. Answering.\n", msg->frame_id.data);
-// 		RCSOFTCHECK(rcl_publish(&pong_publisher, (const void*)msg, NULL));
-// 	}
-// }
-
-
-// void pong_subscription_callback(const void * msgin)
-// {
-// 	const std_msgs__msg__Header * msg = (const std_msgs__msg__Header *)msgin;
-
-// 	if(strcmp(outcoming_ping.frame_id.data, msg->frame_id.data) == 0) {
-// 			pong_count++;
-// 			printf("Pong for seq %s (%d)\n", msg->frame_id.data, pong_count);
-// 	}
-// }
-
-
-// int main()
-// {
-// 	rcl_allocator_t allocator = rcl_get_default_allocator();
-// 	rclc_support_t support;
-
-// 	// create init_options
-// 	RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
-
-// 	// create node
-// 	rcl_node_t node;
-// 	RCCHECK(rclc_node_init_default(&node, "pingpong_node", "", &support));
-
-// 	// Create a reliable ping publisher
-// 	RCCHECK(rclc_publisher_init_default(&ping_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/ping"));
-
-// 	// Create a best effort pong publisher
-// 	RCCHECK(rclc_publisher_init_best_effort(&pong_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/pong"));
-
-// 	// Create a best effort ping subscriber
-// 	RCCHECK(rclc_subscription_init_best_effort(&ping_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/ping"));
-
-// 	// Create a best effort  pong subscriber
-// 	RCCHECK(rclc_subscription_init_best_effort(&pong_subscriber, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Header), "/microROS/pong"));
-
-
-// 	// Create a 3 seconds ping timer timer,
-// 	rcl_timer_t timer;
-// 	RCCHECK(rclc_timer_init_default(&timer, &support, RCL_MS_TO_NS(2000), ping_timer_callback));
-
-
-// 	// Create executor
-// 	rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
-// 	RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator));
-// 	RCCHECK(rclc_executor_add_timer(&executor, &timer));
-// 	RCCHECK(rclc_executor_add_subscription(&executor, &ping_subscriber, &incoming_ping, &ping_subscription_callback, ON_NEW_DATA));
-// 	RCCHECK(rclc_executor_add_subscription(&executor, &pong_subscriber, &incoming_pong, &pong_subscription_callback, ON_NEW_DATA));
-
-// 	// Create and allocate the pingpong messages
-
-// 	char outcoming_ping_buffer[STRING_BUFFER_LEN];
-// 	outcoming_ping.frame_id.data = outcoming_ping_buffer;
-// 	outcoming_ping.frame_id.capacity = STRING_BUFFER_LEN;
-
-// 	char incoming_ping_buffer[STRING_BUFFER_LEN];
-// 	incoming_ping.frame_id.data = incoming_ping_buffer;
-// 	incoming_ping.frame_id.capacity = STRING_BUFFER_LEN;
-
-// 	char incoming_pong_buffer[STRING_BUFFER_LEN];
-// 	incoming_pong.frame_id.data = incoming_pong_buffer;
-// 	incoming_pong.frame_id.capacity = STRING_BUFFER_LEN;
-
-// 	device_id = rand();
-
-// 	rclc_executor_spin(&executor);
-	
-// 	RCCHECK(rcl_publisher_fini(&ping_publisher, &node));
-// 	RCCHECK(rcl_publisher_fini(&pong_publisher, &node));
-// 	RCCHECK(rcl_subscription_fini(&ping_subscriber, &node));
-// 	RCCHECK(rcl_subscription_fini(&pong_subscriber, &node));
-// 	RCCHECK(rcl_node_fini(&node));
-// }
+void loop() {
+  // Spin executor to handle incoming messages
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+  // Add a small delay to avoid tight looping
+  delay(10);
+}
