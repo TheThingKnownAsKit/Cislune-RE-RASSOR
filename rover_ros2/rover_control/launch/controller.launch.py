76017798cd68506142aff32f4e254d73bda83ede
TheThingKnownAsKit <moodyellam@gmail.com>
2025-07-14 20:52:40 +0000

    got twist_mux working for gazebo.launch.py

--- rover_ros2/rover_control/launch/controller.launch.py
+++ rover_ros2/rover_control/launch/controller.launch.py
@@ -1,44 +1,80 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler, TimerAction
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, Command
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.event_handlers import OnProcessStart
 from launch.conditions import IfCondition
+from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
 
     pkg_control = get_package_share_directory('rover_control')
-
     use_sim_time = LaunchConfiguration('use_sim_time')
+    diff_cont_file = os.path.join(pkg_control,'config','diff_cont.yaml')
+
+    robot_description_content = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    robot_description = ParameterValue(robot_description_content, value_type=str)
+    twist_mux_params = os.path.join(pkg_control, 'config', 'twist_mux.yaml')
 
     # Create joy node for joystick input
     teleop_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
-            PathJoinSubstitution([pkg_control, 'launch', 'teleop.launch.py'])
+            PathJoinSubstitution([pkg_control, 'launch', 'joystick.launch.py'])
         ]),
         launch_arguments={'use_sim_time': use_sim_time}.items(),
         condition=IfCondition(LaunchConfiguration('use_teleop'))
     )
 
-    # Create joint broadcaster node for /joint_states
-    joint_broad_node = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=[
-            'joint_state_broadcaster', '-c', '/controller_manager'
-    ])
+    # Twist mux helps manage the cmd_vel topic
+    twist_mux = Node(
+        package="twist_mux",
+        executable="twist_mux",
+        parameters=[twist_mux_params],
+        remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel')]
+    )
 
-    # This is the actual controller manager
-    diff_cont_node = Node(
+    controller_manager = Node(
         package='controller_manager',
-        executable='spawner',
-        arguments=['diff_cont', '-c', '/controller_manager',
-            '--param-file', PathJoinSubstitution([
-                pkg_control, 'controllers', 'diff_cont.yaml'
-            ])
-        ],
-        parameters=[{'use_sim_time': use_sim_time}]
+        executable='ros2_control_node', # Changed
+        parameters=[{
+            'use_sim_time': use_sim_time,
+            'robot_description': robot_description
+            }, diff_cont_file
+        ]
+    )
+    
+    diff_drive = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"]
+    )
+
+    joint_broad = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"]
+    )
+
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive]
+        )
+    )
+
+    delayed_joint_broad = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad]
+        )
     )
 
     return LaunchDescription([
@@ -46,6 +82,8 @@ def generate_launch_description():
         DeclareLaunchArgument('use_teleop', default_value='false'),
 
         teleop_node,
-        joint_broad_node,
-        diff_cont_node
+        twist_mux,
+        delayed_controller_manager,
+        delayed_diff_drive_spawner,
+        delayed_joint_broad
     ])
--- rover_ros2/rover_sim/launch/gazebo.launch.py
+++ rover_ros2/rover_sim/launch/gazebo.launch.py
@@ -12,23 +12,13 @@ def generate_launch_description():
     pkg_description = get_package_share_directory('rover_description')
     pkg_sim = get_package_share_directory('rover_sim')
     pkg_rosgz = get_package_share_directory('ros_gz_sim')
+    pkg_control = get_package_share_directory('rover_control')
 
 
     # ----- Files
     world_file = PythonExpression(["'", LaunchConfiguration('world'), ".world'"])
     world_path = PathJoinSubstitution([pkg_sim, 'worlds', world_file])
     bridge_config_path = PathJoinSubstitution([pkg_sim, 'config', 'rosgz_bridge.yaml'])
-
-    # expand Xacro → URDF *once* and write to a temp file
-    #   (literally the only reason we need to do this is because RosGzBridge is bugged)
-    xacro_file = os.path.join(pkg_description, 'urdf', 'rerassor.xacro.urdf')
-    urdf_xml   = xacro.process_file(xacro_file, mappings={'use_ros2_control': 'true', 'use_gazebo': 'true'}).toxml()
-    tmp_urdf   = tempfile.NamedTemporaryFile(delete=False,
-                                             suffix='.urdf',
-                                             prefix='rerassor_')
-    tmp_urdf.write(urdf_xml.encode())
-    tmp_urdf.close()                       # keep the file on disk
-    urdf_path = tmp_urdf.name              # path we’ll hand to the spawner
 
 
     # ----- Create nodes
@@ -43,14 +33,14 @@ def generate_launch_description():
                 'use_js_gui': 'false'
             }.items())
 
-    # # Use Gazebo's pre-built sim launching node
+    # Use Gazebo's pre-built sim launching node
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             PathJoinSubstitution([pkg_rosgz, 'launch', 'gz_sim.launch.py'])
-            ]),
-            launch_arguments={
-                'gz_args': ['-r ', world_path]
-            }.items())
+        ]),
+        launch_arguments={'gz_args': ['-r ', world_path]}.items()
+    )
+
 
     # Create a node for the rosgz bridge (there is an object named RosGzBridge that does this but it's bugged)  
     rosgz_bridge = Node(
@@ -61,22 +51,51 @@ def generate_launch_description():
     )
     
     # Use Gazebo's pre-built robot spawner
-    spawn_rover = IncludeLaunchDescription(
+    spawn_rover = Node(
+        package='ros_gz_sim',
+        executable='create',
+        name='spawn_rerassor',
+        arguments=[
+            '-world', LaunchConfiguration('world'),
+            '-name',  'rerassor',
+            '-topic', 'robot_description'
+        ],
+        output='screen'
+    )
+
+    diff_drive = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['diff_cont', '-c', '/controller_manager',
+            '--param-file', PathJoinSubstitution([
+                pkg_control, 'config', 'diff_cont.yaml'
+            ])
+        ]
+    )
+
+    joint_broad = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=[
+            'joint_state_broadcaster', '-c', '/controller_manager'
+    ])
+
+    teleop_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
-            PathJoinSubstitution([pkg_rosgz, 'launch', 'gz_spawn_model.launch.py'])
-            ]),
-            launch_arguments={
-                'world': LaunchConfiguration('world'),
-                'file': urdf_path,
-                'entity_name': 'rerassor'
-            }.items())
+            PathJoinSubstitution([pkg_control, 'launch', 'joystick.launch.py'])
+        ]),
+        launch_arguments={'use_sim_time': 'true'}.items()
+    )
 
     return LaunchDescription([
         DeclareLaunchArgument('world', default_value='empty_plane'),
 
         rsp,
         gz_sim,
+        rosgz_bridge,
         spawn_rover,
-        rosgz_bridge
+        diff_drive,
+        joint_broad,
+        teleop_node
     ])
 
